2f2c7ec67b221fbc7356259b1f17b8bca636154e
seglo <seglo@randonom.com>
2015-02-22 18:27:33 -0500

    Co-occurring scala tag counts with post type id filter

--- src/main/scala/StackAnalysis.scala
+++ src/main/scala/StackAnalysis.scala
@@ -9,37 +9,42 @@ object StackAnalysis {
     val sc = new SparkContext(conf)
 
     //val file = sc.textFile("/home/seglo/stackexchange/stackoverflow.com-Posts/Posts.xml")
-    val file = sc.textFile("/home/seglo/stackexchange/stackoverflow.com-Posts/Posts1m.xml")
-    //val file = sc.textFile("/home/seglo/stackexchange/stackoverflow.com-Posts/Posts100k.xml")
+    //val file = sc.textFile("/home/seglo/stackexchange/stackoverflow.com-Posts/Posts1m.xml")
+    val file = sc.textFile("/home/seglo/stackexchange/stackoverflow.com-Posts/Posts100k.xml")
 
     // use random sampling for 10% of data
     //.sample(false, 0.1, System.currentTimeMillis().toInt)
     val tagCounts = file
       .filter(l ⇒ l.contains("row")) // skip XML lines without <row /> elements
-      .flatMap ( l ⇒ {
+      .flatMap( l ⇒ {
         try {
           val xml = XML.loadString(l)
           //val id = (xml \ "@Id").text.toLong
-          //val postTypeId = (xml \ "@PostTypeId").text.toInt
+          val postTypeId = (xml \ "@PostTypeId").text.toInt
           val tags = (xml \ "@Tags").text
-          val splitTags = if (tags.length == 0) Array[String]() else tags.substring(1,tags.length-1).split("><") 
-          val otherTags = splitTags.diff(List("scala"))
-          if (splitTags.length > otherTags.length)
-            otherTags.map(tag ⇒ (tag, 1))
-          else
-            List[(String, Int)]()
+          List((postTypeId, tags))
         } catch {
           case ex: Exception ⇒ {
             println(s"failed to parse line: $l")
-            List[(String, Int)]()
+            List[(Int, String)]()
           }
         }
       })
-        .reduceByKey(_ + _)
-        .collect.toList.sortBy{case (tag, count) ⇒ -count}//.take(10)
-        //.saveAsTextFile("/home/seglo/stackexchange/stackoverflow.com-Posts/tag-counts.txt")
-        //println(s"there are $lineCount lines") 
-        tagCounts.foreach(println)
+      .filter { case (postTypeId, tags) ⇒ postTypeId == 1 } // post type id 1 is a question
+      .flatMap { case (postTypeId, tags) ⇒ {
+        val splitTags = if (tags.length == 0) Array[String]() else tags.substring(1,tags.length-1).split("><") 
+        val otherTags = splitTags.diff(List("scala"))
+        if (splitTags.length > otherTags.length)
+          otherTags.map(tag ⇒ (tag, 1))
+        else
+          List[(String, Int)]() 
+      }}
+      .reduceByKey((a, b) ⇒ a + b)
+      .collect.toList.sortBy{case (tag, count) ⇒ -count}//.take(10)
+      //.saveAsTextFile("/home/seglo/stackexchange/stackoverflow.com-Posts/tag-counts.txt")
+   
+    //println(s"there are $lineCount lines") 
+    tagCounts.foreach(println)
 
   }
 }
