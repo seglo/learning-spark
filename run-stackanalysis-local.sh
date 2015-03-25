e2f55f3b08044b11d1b6ff8a1b58c34106a3f793
seglo <seglo@randonom.com>
2015-03-24 22:18:28 -0400

    Use * cores locally instead of fixed

--- run-stackanalysis-local.sh
+++ run-stackanalysis-local.sh
@@ -4,7 +4,7 @@ SPARK_HOME=/usr/local/spark
 # todo: construct hdfs file format to make relative 
 
 # run locally on 8 cores
-$SPARK_HOME/bin/spark-submit --class "StackAnalysis" --master local[8] target/scala-2.10/learning-spark_2.10-0.1.0.jar \
-  --input-file file:///home/seglo/source/learning-spark/data/stackexchange/stackoverflow.com-Posts/Posts100k.xml
+$SPARK_HOME/bin/spark-submit --class "StackAnalysis" --master local[*] target/scala-2.10/learning-spark_2.10-0.1.0.jar \
+  --input-file file:///home/seglo/source/learning-spark/data/stackexchange/stackoverflow.com-Posts/Posts.xml
 # file:///home/seglo/source/learning-spark/data/stackexchange/stackoverflow.com-Posts/Posts1m.xml 
 # file:///home/seglo/source/learning-spark/data/stackexchange/stackoverflow.com-Posts/Posts.xml
