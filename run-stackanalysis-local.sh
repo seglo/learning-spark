bef631f6015638a17a155e3d55cd593e8cd0e0b4
seglo <seglo@randonom.com>
2015-03-28 15:30:21 -0400

    Working redistributable local example

--- run-stackanalysis-local.sh
+++ run-stackanalysis-local.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 SPARK_HOME=/usr/local/spark
 
-# todo: construct hdfs file format to make relative 
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
 
-# run locally on 8 cores
+rm -rf $SCRIPTPATH/data/output/*
+
+# run locally on all cores
 $SPARK_HOME/bin/spark-submit --class "StackAnalysis" --master local[*] target/scala-2.10/learning-spark_2.10-0.1.0.jar \
-  --input-file file:///home/seglo/source/learning-spark/data/stackexchange/stackoverflow.com-Posts/Posts1m.xml \
-  --output-file /home/seglo/source/learning-spark/data/output/StackAnalysisCount.txt
-#  --output-file file:///home/seglo/source/learning-spark/data/output/StackAnalysisCount.txt
-# file:///home/seglo/source/learning-spark/data/stackexchange/stackoverflow.com-Posts/Posts1m.xml 
-# file:///home/seglo/source/learning-spark/data/stackexchange/stackoverflow.com-Posts/Posts.xml
+  --input-file $SCRIPTPATH/data/stackexchange/stackoverflow.com-Posts/Posts100k.xml \
+  --output-directory $SCRIPTPATH/data/output 
--- src/main/scala/StackAnalysis.scala
+++ src/main/scala/StackAnalysis.scala
@@ -1,3 +1,4 @@
+import java.util.Date
 import java.io.File
 import java.text.SimpleDateFormat
 import org.apache.commons.io.FileUtils
@@ -21,20 +22,44 @@ object StackAnalysis {
       val inputFile = options.get('inputfile)
         .getOrElse("data/stackexchange/stackoverflow.com-Posts/Posts100k.xml").toString
 
-      // Optional Output file
-      val outputFile = options.get('outputfile)
+      // Optional directory
+      val outputDir = options.get('outputdir)
 
-      tagCounts(sc, inputFile, outputFile)
-    } finally {
-      sc.stop()
+      try {
+        val rows = sc.textFile(inputFile)
+        val sqs = scalaQuestions(rows)
+        
+        println(s"Scala question count ${sqs.count}") 
+        
+        val tc = tagCounts(sqs)
+
+        val sqsByMonth = scalaQuestionsByMonth(sqs)
+    
+        outputDir match {
+          // NOTE: When writing to HDFS use `hdfs dfs -getmerge` to retrieve fully constructed file
+          case Some(outputDir) ⇒ {
+            println("Writing output files to disk")
+            
+            val outputDirStr = outputDir.toString
+            val stcFile = new File(outputDirStr, "ScalaTagCount.txt").toURI().toURL().toExternalForm()
+            val sqbmFile = new File(outputDirStr, "ScalaQuestionsByMonth.txt").toURI().toURL().toExternalForm() 
+            
+            tc.saveAsTextFile(stcFile)
+            sqsByMonth.saveAsTextFile(sqbmFile)
+          }
+          case None ⇒ println("No output file provided.")
+        }
+      } finally {
+        sc.stop()
+      }
     }
   }
 
-  def questions(sc: SparkContext, inputFile: String) = {
+  def scalaQuestions(rows: RDD[String]) = {
     val sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS")
-    // Use random sampling for 10% of data
-    //.sample(false, 0.1, System.currentTimeMillis().toInt)
-    sc.textFile(inputFile)
+    rows  
+      // Use random sampling for 10% of data
+      //.sample(false, 0.1, System.currentTimeMillis().toInt)
       // Skip XML lines without <row /> elements
       .filter(l ⇒ l.contains("row"))
       // Extract PostTypeId and Tags
@@ -64,10 +89,14 @@ object StackAnalysis {
       }
       // Only get "Question" posts (PostTypeId == 1)
       .filter { case (postTypeId, creationDate, tags) ⇒ postTypeId == 1 }
+      // Filter question posts by those that contain Scala tags
+      .filter { case (id, creationDate, tags) ⇒ tags.contains("scala") }
+      // Cache this dataflow for other uses
+      .cache
   }
 
-  def tagCounts(sc: SparkContext, inputFile: String, outputFile: Option[Any]) = {
-    val tagCounts = questions(sc, inputFile)
+  def tagCounts(qs: RDD[(Int, Date, List[String])]) = {
+    qs
       // If this question contains a scala tag then return a collection of all other tags
       .flatMap { case (postTypeId, creationDate, tags) ⇒ {
         val otherTags = tags.diff(List("scala"))
@@ -85,17 +114,18 @@ object StackAnalysis {
       .map { case (tag, count) ⇒ (count, tag) }
       // Sort by tag counts in descending order
       .sortByKey(false)
-      // Flush datapipeline to memory
-      .collect
-      //.saveAsTextFile("/home/seglo/stackexchange/stackoverflow.com-Posts/tag-counts.txt")
-   
-    outputFile match {
-      // TODO: write non-collected stream to HDFS and use `hdfs dfs -getmerge`
-      case Some(outputFile) ⇒ {
-        println(s"Writing output file ${outputFile.toString} to disk")
-        FileUtils.writeLines(new File(outputFile.toString), tagCounts.toSeq)
-      }
-      case None ⇒ println("No output file provided.")
-    }
+  }
+
+  def scalaQuestionsByMonth(sqs: RDD[(Int, Date, List[String])]) = {
+    // Date pattern to group by (Month)
+    val sdfMonth = new SimpleDateFormat("yyyy-MM")
+    
+    sqs
+      // Transform to key value pairs of Month and Seed count
+      .map { case (id, creationDate, tags) ⇒ (sdfMonth.format(creationDate), 1)} 
+      // Sum reduction by key
+      .reduceByKey((a, b) ⇒ a + b) 
+      // Sort by month
+      .sortByKey(true)
   }
 }
