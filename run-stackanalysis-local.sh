f7a02e1d9c197d5ccf52b0d11807b2b610ed1715
seglo <seglo@randonom.com>
2015-03-28 22:49:48 -0400

    sbt package as part of run script

--- run-stackanalysis-local.sh
+++ run-stackanalysis-local.sh
@@ -7,6 +7,8 @@ SCRIPTPATH=$(dirname "$SCRIPT")
 rm -rf $SCRIPTPATH/data/output/ScalaTagCount.txt
 rm -rf $SCRIPTPATH/data/output/ScalaQuestionsByMonth.txt
 
+sbt package
+
 # run locally on all cores
 $SPARK_HOME/bin/spark-submit --class "StackAnalysis" --master local[*] target/scala-2.10/learning-spark_2.10-0.1.0.jar \
   --input-file $SCRIPTPATH/data/stackexchange/stackoverflow.com-Posts/Posts100k.xml \
